4a46d6e56b71cf149cff62e05a925a5bd22f6962
Luis <luisinhofehalcantara@gmail.com>
2025-10-30 14:22:18 -0300

    commit final

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ function scripts(){
 
 }
 
-exports.default = gulp.parallel(styles, images,scripts); 
+exports.default = gulp.parallel(styles,images,scripts); 
 exports.watch=function (){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
